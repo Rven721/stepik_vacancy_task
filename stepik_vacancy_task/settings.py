97a8ab8dd6e107f7afcedafe7b75129069e6f015
Rven721 <amalyavin1@gmail.com>
2021-04-10 18:40:37 +0300

    final commit

--- stepik_vacancy_task/settings.py
+++ stepik_vacancy_task/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0o#%3l71+d67%4e&f58i$8(bx7l(fju_di!_oh3=s&zrno10=h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
